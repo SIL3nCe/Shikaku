5f0de7f736ca88d4a227cbfc80b14af5d5e43450
S1L3nCe <tom.pradal@aliceadsl.fr>
2020-01-23 13:59:15 +0100

    Init + Grid generation

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    //TODO Make an enum for empty/already in a rectangle/origin case
+    private Vector2Int coordinates;
+    
+    public void Initialize(int x, int y, int size)
+    {
+        coordinates = new Vector2Int(x, y);
+    }
+}
---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+    [Tooltip("Grid size")]
+    public int width;
+    public int height;
+
+    [Tooltip("Object to instantiate")]
+    public GameObject cellPrefab;
+
+    private GameObject[,] aGrid;
+
+    void Start()
+    {
+        aGrid = new GameObject[width,height];
+
+        int cellSize = 10;
+        for (int iHeight = 0; iHeight <= height; iHeight++)
+        {
+            for (int iWidth = 0; iWidth <= width; ++iWidth)
+            {
+                aGrid[iHeight, iWidth] = Instantiate(cellPrefab, new Vector3(iWidth, iHeight, 0), Quaternion.identity);
+                aGrid[iHeight, iWidth].GetComponent<Cell>().Initialize(iHeight, iHeight, cellSize);
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    
+    //}
+}
