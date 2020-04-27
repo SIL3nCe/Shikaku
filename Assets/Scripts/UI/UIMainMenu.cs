4512d4b56ed13451bb2c3c16e70050a86b692383
S1L3nCe <tom.pradal@aliceadsl.fr>
2020-04-27 20:35:20 +0200

    Added Kenney UI assets. Added MainMenu scene & script wip

---
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMainMenu : MonoBehaviour
+{
+    // Play button event
+    public void OnPlayEvent(int level)
+    {
+        // 0>3 : Very easy, easy, medium, hard
+    }
+    
+    public void OnCustomEvent()
+    {
+    }
+
+    public void OnHelpEvent()
+    {
+    }
+
+    public void OnStatisticsEvent()
+    {
+    }
+
+    public void OnOptionsEvent()
+    {
+    }
+}
