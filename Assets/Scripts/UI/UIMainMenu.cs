fa68b174f5051b4126b0bf61bf46ca999575e55c
S1L3nCe <tom.pradal@aliceadsl.fr>
2020-05-21 17:33:30 +0200

    Main menu correctly implemented

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -5,12 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class UIMainMenu : MonoBehaviour
 {
+    // Midle layout with content
+    public GameObject ContentMain;
+    public GameObject ContentOptions;
+    public GameObject ContentHelp;
+    public GameObject ContentStats;
+
+    // Bottom layout with buttons
+    public GameObject ButtonMain;
+    public GameObject ButtonBack;
+
+
     // Play button event
     public void OnPlayEvent(int level)
     {
         // 0>3 : Very easy, easy, medium, hard
         StaticDatas.eCurrentDifficulty = (EDifficulty)level;
-        SceneManager.LoadScene("GameGrid");
+        SceneManager.LoadScene("SampleScene");
     }
     
     public void OnCustomEvent()
@@ -19,13 +30,39 @@ public class UIMainMenu : MonoBehaviour
 
     public void OnHelpEvent()
     {
+        ContentMain.SetActive(false);
+        ButtonMain.SetActive(false);
+
+        ContentHelp.SetActive(true);
+        ButtonBack.SetActive(true);
     }
 
     public void OnStatisticsEvent()
     {
+        ContentMain.SetActive(false);
+        ButtonMain.SetActive(false);
+
+        //ContentStats.SetActive(true);
+        ButtonBack.SetActive(true);
     }
 
     public void OnOptionsEvent()
     {
+        ContentMain.SetActive(false);
+        ButtonMain.SetActive(false);
+
+        ContentOptions.SetActive(true);
+        ButtonBack.SetActive(true);
+    }
+
+    public void OnBackEvent()
+    {
+        //ContentStats.SetActive(false);
+        ContentHelp.SetActive(false);
+        ContentOptions.SetActive(false);
+        ButtonBack.SetActive(false);
+
+        ContentMain.SetActive(true);
+        ButtonMain.SetActive(true);
     }
 }
