015866443d4abf3132612f7313aea693eb2538ff
Johann NOVAK <johann.novak25@gmail.com>
2020-06-07 09:21:32 +0200

    Added GameGUIInputHandler

---
+++ Assets/Scripts/InputsHandler/GameGUIInputsHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGUIInputsHandler : InputsHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+	}
+
+	public override void HandleInputs(Vector2 vScreenPosition)
+	{
+
+	}
+
+	public override void InputsStopped()
+	{
+
+	}
+}
