17b6c0143f8d68f2536faa853868e8f13fc0ac0e
Johann NOVAK <johann.novak25@gmail.com>
2020-06-07 11:15:59 +0200

    Update CanvasGameInputsHandler.cs

--- Assets/Scripts/InputsHandler/CanvasGameInputsHandler.cs
+++ Assets/Scripts/InputsHandler/CanvasGameInputsHandler.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasGameInputsHandler : InputsHandler
 {
 	private GamePlaySelection m_GamePlaySelection;
 	private Canvas m_CanvasGame;
 	private Canvas m_CanvasGUI;
+	private RawImage m_ImageCameraGame;
 	private CameraInputsHandler m_CameraInputsHandler;
+
 	private Vector2 m_vInputPosition;
 	private Vector2 m_vInputPositionTransformed;
 
@@ -31,19 +34,32 @@ public class CanvasGameInputsHandler : InputsHandler
 		m_CameraInputsHandler = cameraInputsHandler;
 	}
 
+	public void SetImageCameraGame(RawImage image)
+	{
+		m_ImageCameraGame = image;
+	}
+
 	public override void HandleInputs(Vector2 vScreenPosition)
 	{
 		m_vInputPosition = vScreenPosition;
 		RectTransform rectTransform = m_CanvasGame.GetComponent<RectTransform>();
 
 		//
+		// Offset first
+		RectTransform rectTransformImageCameraGame = m_ImageCameraGame.GetComponent<RectTransform>();
+		Vector2 vPositionOffset = vScreenPosition;
+
+		//
 		// Ratio between two canvas size. Simplification here as initial orthographic size of 
 		// game camera = size of game canvas. And zoom modifies GameCamera ortho size.
-		Vector2 vRatioCanvas = new Vector2(	(m_CanvasGame.worldCamera.orthographicSize * 2.0f) / m_CanvasGUI.GetComponent<RectTransform>().rect.width,
-											(m_CanvasGame.worldCamera.orthographicSize * 2.0f) / m_CanvasGUI.GetComponent<RectTransform>().rect.height);
+		RectTransform rectTransformCanvasGUI = m_CanvasGUI.GetComponent<RectTransform>();
+		Vector2 vRatioCanvas = new Vector2(	(m_CanvasGame.worldCamera.orthographicSize * 2.0f) / rectTransformCanvasGUI.rect.size.x,
+											(m_CanvasGame.worldCamera.orthographicSize * 2.0f) / rectTransformCanvasGUI.rect.size.y);
 
 		m_vInputPositionTransformed =	m_CameraInputsHandler.GetPanning() +
-										vRatioCanvas * vScreenPosition;
+										vRatioCanvas * vPositionOffset
+										//+ rectTransformImageCameraGame.offsetMax
+										;
 		//m_vInputPositionTransformed = new Vector2(m_CanvasGame.worldCamera.transform.localPosition.x, m_CanvasGame.worldCamera.transform.localPosition.y) + vScreenPositionWithModifiers / fZoomRatio;
 		m_GamePlaySelection.UpdateInputPosition(m_vInputPositionTransformed);
 	}
